62e38d1ec5fa4ffd99c427a8ea4c4bb829015328
rjsdn1120 <rjsdn1120@gmail.com>
2023-05-19 01:09:59 +0900

    remove unused variables

--- leetcode/55.py
+++ leetcode/55.py
@@ -1,6 +1,6 @@
 class Solution:
 
-    def f(self, x, nums,check,dp):
+    def f(self, x, nums,check):
         # print(x,check,dp)
         if check[len(nums)-1]:
             return
@@ -8,12 +8,11 @@ class Solution:
             if x+i >= len(nums): continue
             if check[x+i]: continue
             check[x+i]=True
-            self.f(x+i,nums,check,dp)
+            self.f(x+i,nums,check)
 
     def canJump(self, nums: List[int]) -> bool:
         check = [False] * len(nums)
-        dp = [0] * len(nums)
         check[0]=True
-        self.f(0,nums,check,dp)
+        self.f(0,nums,check)
         return check[len(nums)-1]
         
